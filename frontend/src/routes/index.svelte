4efdeee68ddc2978eb5f767bf010627c5e13a83d
omar2205 <omar2205a@yahoo.com>
2022-06-17 11:54:11 +0000

    add sveltekit empty static project dynamic backend URL using env vars during build time

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import { variables } from '../variables'
+
+  let res = 'Loading...'
+
+  onMount(() => {
+    fetch(variables.BACKEND_API_URL)
+    .then(r => r.json())
+    .then(r => res = r)
+  })
+</script>
+
+<h1>Welcome to SvelteKit!</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+{#if res === 'Loading...'}
+  <p>Loading...</p>
+{:else}
+<pre>
+{JSON.stringify(res, null, 2)}
+</pre>
+{/if}
---
+++ frontend/src/variables.ts
@@ -0,0 +1,3 @@
+export const variables = {
+  BACKEND_API_URL: import.meta.env.VITE_BACKEND_API_URL
+}
